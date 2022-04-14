f0d15b295afcfc48c3dc6b0b7e0934202a383cfb
Brian Tam <tamwinghei@Brians-MacBook-Pro.local>
2022-04-14 16:11:00 -0700

    updated post and patch request

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { all } = require('express/lib/application');
 const path = require('path');
+const { v4: uuid } = require('uuid')
+
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,18 +14,22 @@ app.set('views', path.join(__dirname, 'views'))
 
 const comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol its so funny!',
     },
     {
+        id: uuid(),
         username: 'damn',
         comment: 'bruh haha'
     },
     {
+        id: uuid(),
         username: 'brian',
         comment: 'xdxd',
     },
     {
+        id: uuid(),
         username: 'katie',
         comment: 'so tidy'
     }
@@ -32,8 +39,32 @@ app.get('/comments', (req, res) => {
     res.render('comments/index', { comments })
 })
 
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body
+    comments.push({ username, comment, id: uuid(), })
+    res.redirect('/comments');
+})
+
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/show', { comment, id })
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const FoundComment = comments.find(c => c.id === id);
+    FoundComment.comment = newCommentText;
+    res.redirect('comments')
 
 })
 
